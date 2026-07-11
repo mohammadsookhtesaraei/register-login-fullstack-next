e74b576e1b6ba66b05acd39be5caea9c24d84b2e
mohammad sookhtesaraei <114998362+mohammadsookhtesaraei@users.noreply.github.com>
2026-07-11 15:40:05 +0330

    add auth page

---
+++ src/app/(auth)/auth/page.tsx
@@ -0,0 +1,23 @@
+"use client";
+
+import Login from "@/app/(auth)/auth/components/Login";
+import Register from "@/app/(auth)/auth/components/Register";
+import { useState } from "react";
+
+const Auth = () => {
+  const [step, setStep] = useState(1);
+
+  const authRender = (steps: number) => {
+    switch (steps) {
+      case 1:
+        return <Register />;
+      case 2:
+        return <Login />;
+    }
+  };
+
+  return <div>
+    {authRender(step)}
+  </div>;
+};
+export default Auth;
